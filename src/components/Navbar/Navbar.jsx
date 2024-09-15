476ac66ec0b8938fb65d9504731bfe35b3d66e10
Sourav Jaiswal <jaiswalsourav5@gmail.com>
2024-09-15 14:32:45 +0530

    Improt frontend admin panel from some unkown place.

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import './Navbar.css';
+import { assets } from '../../assets/assets';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-content">
+        <div className="navbar-left">
+          <span className="company-name">Foodies</span>
+          <sub className="admin-panel">Admin Panel</sub> {/* Subscript for Admin Panel */}
+          <img className="logo" src={assets.logo} alt="Logo" />
+        </div>
+        <div className="navbar-right">
+          <img className="profile" src={assets.profile_image} alt="Profile" />
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
